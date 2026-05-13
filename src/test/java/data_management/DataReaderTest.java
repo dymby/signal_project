26867b3fe60cb41d344f9313eeb0eb0eb67c9b62
dymby <dimimihai2006@gmail.com>
2026-05-13 15:17:47 +0200

    solved DataReaderTest.java errors

--- src/test/java/data_management/DataReaderTest.java
+++ src/test/java/data_management/DataReaderTest.java
@@ -1,29 +1,33 @@
 package data_management;
 
 import com.data_management.DataStorage;
-import com.data_management.PatientRecord;
 import com.data_management.DataReader;
-import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class DataReaderTest implements DataReader{
 
+
     @Override
     public void readData(DataStorage dataStorage) throws IOException {
 
-        Scanner scanner = new Scanner(new File("src/test/resources/MockInput.txt"));
+        try {
+            InputStreamReader reader = new FileReader(new File("src/test/resources/MockInput.txt"));
+            Scanner scanner = new Scanner(reader);
+                
+            while (scanner.hasNextLine()) {
+                int patientId = scanner.nextInt();
+                double measurementValue = scanner.nextDouble();
+                long timestamp = scanner.nextLong();
+                String recordType = scanner.next();
+                dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
+                }
 
-        while (scanner.hasNextLine()) {
-            int patientId = scanner.nextInt();
-            double measurementValue = scanner.nextDouble();
-            long timestamp = scanner.nextLong();
-            String recordType = scanner.next();
-
-            dataStorage.addPatientData(patientId, measurementValue, recordType, timestamp);
-        }
+            scanner.close();
+        } catch (Exception e) { e.printStackTrace(); }
     }
 }
